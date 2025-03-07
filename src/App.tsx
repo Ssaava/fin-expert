930bc43c9b905451a308db10752118e6544fdf8e
Ssaava <ssavaemma4@gmail.com>
2025-03-07 16:55:00 +0300

    added survey intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Route, Routes } from "react-router";
-import LandingPageLayout from "./layouts/landing-page-layout";
-import AuthLayout from "./layouts/auth-layout";
-import ManagePasswordLayout from "./layouts/manage-password-layout";
-import SurveyLayout from "./layouts/survey-layout";
 import Home from "@/pages/home";
-import Insights from "./pages/insights";
+import { Route, Routes } from "react-router";
+import AuthLayout from "./layouts/auth-layout";
+import LandingPageLayout from "./layouts/landing-page-layout";
+import ManagePasswordLayout from "./layouts/manage-password-layout";
 import SurveyInsightsLayout from "./layouts/survey-insights-layout";
+import Insights from "./pages/insights";
+import Survey from "./pages/survey";
 
 const App = () => {
   return (
@@ -13,7 +13,9 @@ const App = () => {
       <Route path="/" element={<LandingPageLayout />}>
         <Route index element={<Home />} />
       </Route>
-      <Route path="/survey" element={<SurveyLayout />}></Route>
+      <Route path="/survey">
+        <Route index element={<Survey />} />
+      </Route>
       <Route path="/survey-insights" element={<SurveyInsightsLayout />}>
         <Route index element={<Insights />} />
       </Route>
