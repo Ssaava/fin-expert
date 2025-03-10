74ab8e41f20d979e13d825d7715107c35f778629
Ssaava <ssavaemma4@gmail.com>
2025-03-10 20:07:14 +0300

    added bg assets on home page

--- src/components/home/ExpertFlowSection.tsx
+++ src/components/home/ExpertFlowSection.tsx
@@ -1,16 +1,28 @@
 import city from "/city.png";
 import insights from "/insights.png";
+import bgImage from "/transform.png";
+
 const ExpertFlowSection = () => {
   return (
     <>
       <section className="px-4 md:px-10 min-h-screen">
-        <div className="gradient-yellow-bg px-4 md:px-16 xl:px-56 py-10 md:py-16 rounded-3xl mt-12 flex flex-col gap-16">
+        <div className="relative gradient-yellow-bg px-4 md:px-16 xl:px-56 py-10 md:py-16 rounded-3xl mt-12 flex flex-col gap-16">
+          <div className="absolute top-0 left-0 right-0 bottom-0">
+            <img
+              src={bgImage}
+              alt="Hero bg"
+              className="block w-full h-full object-cover"
+            />
+          </div>
           <h3 className="text-center font-medium text-3xl md:text-5xl ">
             Struggling to measure financial inclusivity?
           </h3>
 
-          <div className="grid md:grid-cols-2 justify-between gap-6 md:gap-10 lg:gap-20">
-            <div className="rounded-lg bg-white/70 p-6 md:p-12 flex flex-col gap-4">
+          <div className="grid md:grid-cols-2 justify-between gap-20">
+            <div className="relative z-10 rounded-lg bg-white/70 p-6 md:p-12 flex flex-col gap-4">
+              <div className="absolute md:-right-20 md:top-1/2 md:-translate-y-1/2 max-md:left-1/2 max-md:-translate-x-1/2 max-md:rotate-90 max-md:-bottom-12">
+                <img src="/connector-line-right.png" alt="" />
+              </div>
               <p>1.</p>
               <h3 className="text-2xl font-medium">Signup on the platform</h3>
               <p className="text-black/70 font-medium">
@@ -25,7 +37,19 @@ const ExpertFlowSection = () => {
                 </div>
               </div>
             </div>
-            <div className="rounded-lg bg-white/70 p-6 md:p-12 flex flex-col gap-4">
+            <div className="relative z-10 rounded-lg bg-white/70 p-6 md:p-12 flex flex-col gap-4">
+              <div className="absolute md:-bottom-20 md:-left-20 max-md:left-1/2 max-md:-translate-x-1/2 max-md:rotate-90 max-md:-bottom-12">
+                <img
+                  src="/connector-line-bottom.png"
+                  alt=""
+                  className="hidden md:block"
+                />
+                <img
+                  src="/connector-line-right.png"
+                  alt=""
+                  className="hidden max-md:block"
+                />
+              </div>
               <p>2.</p>
               <h3 className="text-2xl font-medium">Complete assessment</h3>
               <p className="text-black/70 font-medium">
@@ -35,7 +59,7 @@ const ExpertFlowSection = () => {
                 <img src={city} alt="City Image" />
               </div>
             </div>
-            <div className="md:col-span-2 ">
+            <div className="z-10 md:col-span-2 ">
               <div className="w-fit mx-auto bg-white/70 rounded-lg  p-6 md:p-12 flex flex-col gap-4">
                 <p>3.</p>
                 <h3 className="text-2xl font-medium">
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import MetricsSection from "@/components/home/MetricsSection";
 import ExpertFlowSection from "@/components/home/ExpertFlowSection";
 import FaqSection from "@/components/home/FaqSection";
 import { Button } from "@/components/ui/button";
+import bgImage from "/inclusivity.png";
 const Home = () => {
   return (
     <>
@@ -16,7 +17,14 @@ const Home = () => {
 
       <FaqSection />
 
-      <section className="px-4 md:px-10 mt-8 lg:mt-0 mb-12">
+      <section className="relative px-4 md:px-10 mt-8 lg:mt-0 mb-12">
+        <div className="absolute top-0 left-0 right-0 bottom-0">
+          <img
+            src={bgImage}
+            alt="Hero bg"
+            className="block w-full h-full object-cover"
+          />
+        </div>
         <div className="gradient-light-blue-bg px-4 md:px-16 xl:px-56 py-10 md:py-16 rounded-3xl flex flex-col gap-16">
           <h3 className="text-center text-white text-3xl md:text-6xl">
             Ready to enhance financial inclusivity?
